c13aa5205eb13e7f0acb67a5a027e8e114e4aed8
Alex Moller <amloexler@gmail.com>
2015-08-24 18:33:58 -0700

    commit before I mess with stuff

--- Favor/Response.m
+++ Favor/Response.m
@@ -10,6 +10,15 @@
 
 @implementation Response
 
+@dynamic responseCreatorName;
+@dynamic responseCreatorProfileImage;
+@dynamic timeAgo;
+@dynamic phoneNumber;
+@dynamic responseText;
+@dynamic profPicFile;
+@dynamic userWhoMadeThisResponse;
+@dynamic favorThisResponseIsOn;
+
 
 + (NSString *)parseClassName
 {
